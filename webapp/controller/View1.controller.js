026b7278d71ec0a1956fdab825b4af17dd3afec4
evertdutoit <evert.du.toit@us.epiuse.com>
2020-01-05 17:02:52 +0000

    initial

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("edt.dragButton.controller.View1", {
+		onInit: function () {
+
+		},
+		
+		onDropButton: function(oEvt) {
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"edt/dragButton/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
